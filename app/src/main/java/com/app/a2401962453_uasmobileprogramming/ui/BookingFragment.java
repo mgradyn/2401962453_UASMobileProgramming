0ed53906aaf7eecb625197802a106d093326eb0b
mgradyn <mgradyn@gmail.com>
2023-02-04 19:20:28 +0700

    doneeee

--- app/src/main/java/com/app/a2401962453_uasmobileprogramming/ui/BookingFragment.java
+++ app/src/main/java/com/app/a2401962453_uasmobileprogramming/ui/BookingFragment.java
@@ -10,13 +10,16 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.app.a2401962453_uasmobileprogramming.MainActivity;
 import com.app.a2401962453_uasmobileprogramming.R;
@@ -24,6 +27,8 @@ import com.app.a2401962453_uasmobileprogramming.adapter.CinemaDateListAdapter;
 import com.app.a2401962453_uasmobileprogramming.adapter.CinemaRoomListAdapter;
 import com.app.a2401962453_uasmobileprogramming.adapter.CinemaTimeListAdapter;
 import com.app.a2401962453_uasmobileprogramming.databinding.FragmentBookingBinding;
+import com.app.a2401962453_uasmobileprogramming.model.CinemaDate;
+import com.app.a2401962453_uasmobileprogramming.model.CinemaTime;
 import com.app.a2401962453_uasmobileprogramming.model.Result;
 import com.app.a2401962453_uasmobileprogramming.tool.OnDateCardClickListener;
 import com.app.a2401962453_uasmobileprogramming.tool.OnRoomCardClickListener;
@@ -31,6 +36,7 @@ import com.app.a2401962453_uasmobileprogramming.tool.OnTimeCardClickListener;
 import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class BookingFragment extends Fragment implements OnRoomCardClickListener, OnDateCardClickListener, OnTimeCardClickListener {
@@ -42,7 +48,10 @@ public class BookingFragment extends Fragment implements OnRoomCardClickListener
     private CinemaDateListAdapter cinemaDateListAdapter;
     private CinemaTimeListAdapter cinemaTimeListAdapter;
 
-    private String roomType;
+    private String cinemaLocation = null;
+    private String roomType = null;
+    private CinemaDate cinemaDate = null;
+    private CinemaTime cinemaTime = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -82,6 +91,60 @@ public class BookingFragment extends Fragment implements OnRoomCardClickListener
         setCinemaDateAdapter();
         setCinemaTimeAdapter();
 
+        binding.btnProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Bundle bundle = new Bundle();
+//                bundle.putParcelable("movieDetail",movieDetail);
+
+                String name = binding.etName.getText().toString();
+                String email = binding.etEmail.getText().toString();
+                boolean timeHasPass = false;
+                if (cinemaDate != null && cinemaTime != null) {
+                    Date selectedDate = cinemaDate.getFullDate();
+                    int hour = cinemaTime.getHour();
+                    int minute = cinemaTime.getMinute();
+                    Date currentDate = new Date();
+
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(selectedDate);
+                    calendar.set(Calendar.HOUR_OF_DAY, hour);
+                    calendar.set(Calendar.MINUTE, minute);
+                    selectedDate = calendar.getTime();
+
+                    if (selectedDate.compareTo(currentDate) <= 0) {
+                        timeHasPass = true;
+                    }
+                }
+
+                if (TextUtils.isEmpty(name)) {
+                    makeToast("Please enter name");
+                }
+                else if (!isValidEmail(email)) {
+                    makeToast("Please enter valid email");
+                }
+                else if (cinemaLocation == null) {
+                    makeToast("Please select the location");
+                }
+                else if (roomType == null) {
+                    makeToast("Please select the room");
+                }
+                else if (cinemaDate == null) {
+                    makeToast("Please select the date");
+                }
+                else if (cinemaTime == null) {
+                    makeToast("Please select the time");
+                }
+                else if (timeHasPass) {
+                    makeToast("Please select another time");
+                }
+                else {
+                    makeToast("Booking Success");
+                    Navigation.findNavController(view).navigate(R.id.action_bookingFragment_to_homeFragment);
+                }
+            }
+        });
+
         super.onViewCreated(view, savedInstanceState);
     }
 
@@ -109,7 +172,8 @@ public class BookingFragment extends Fragment implements OnRoomCardClickListener
 
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+                cinemaLocation = adapterView.getItemAtPosition(i).toString();
+                Log.d("cinemaLocation", cinemaLocation);
             }
 
             @Override
@@ -155,6 +219,7 @@ public class BookingFragment extends Fragment implements OnRoomCardClickListener
         Log.d("cinemaDate", date);
         Log.d("cinemaDay", day);
         Log.d("cinemaFullDate", fullDate.toString());
+        this.cinemaDate = new CinemaDate(fullDate);
     }
 
     @Override
@@ -162,6 +227,17 @@ public class BookingFragment extends Fragment implements OnRoomCardClickListener
         Log.d("cinemaTime", time);
         Log.d("cinemaHour", String.valueOf(hour));
         Log.d("cinemaMinute", String.valueOf(minute));
+        this.cinemaTime = new CinemaTime(hour, minute);
 
+    }
+
+    private void makeToast(String msg) {
+        Toast.makeText(
+                getContext(), msg, Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    private boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }
